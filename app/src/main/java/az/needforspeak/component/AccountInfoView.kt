893cfb4bfe6ac93bce46e7b93066115e03bc3798
Ferzin Zeynal <ferzo.zeynal@mail.ru>
2022-12-26 23:11:52 +0400

    account info item added

---
+++ app/src/main/java/az/needforspeak/component/AccountInfoView.kt
@@ -0,0 +1,29 @@
+package az.needforspeak.component
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import az.needforspeak.R
+
+class AccountInfoView (context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    private var layout: View = inflate(context, R.layout.account_info_item, this)
+
+
+    init {
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.LanguagesView)
+        //icon.setImageDrawable(attributes.getDrawable(R.styleable.LanguagesView_languagesImage))
+
+    }
+
+    fun setOnClick(click: OnClickListener) {
+       // layout.setOnClickListener(click)
+    }
+
+    fun setIconVisibility(visibility: Int) {
+      //  select.visibility = visibility
+    }
+
+}
